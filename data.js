ba013b486100221a90f12cc924ebc000a7bc98d0
Madison Law <madisonlaw1@gmail.com>
2022-03-31 16:46:01 -0600

    Add data file

---
+++ data.js
@@ -0,0 +1,19 @@
+const starredMovies = [
+  {
+    "id": 694919,
+    "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+    "title": "Money Plane",
+  },
+  {
+    "id": 337401,
+    "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+    "title": "Mulan",
+  },
+  {
+    "id": 718444,
+    "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+    "title": "Rogue",
+  }];
+
+
+  module.exports = { starredMovies }
